c30a512e3d4c16936dec10c13127b199b154c3c4
huaxin <huaxin@xiongmaitech.com>
2019-07-15 14:05:04 +0800

    20190715 14:05

--- init.el
+++ init.el
@@ -23,6 +23,21 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq mouse-yank-at-point t)
 
+(defun show-buffer-file-name ()
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if file-name
+        (progn
+          (message file-name)
+          (kill-new file-name)))))
+(global-set-key (kbd "C-c C-f") 'show-buffer-file-name)
+
+(setq url-automatic-caching t)
+(global-set-key (kbd "C-c i") 'youdao-dictionary-search-from-input)
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
+
+(global-set-key (kbd "M-o") 'ace-window)
+
 (global-set-key (kbd "C-\\") #'helm-imenu-anywhere)
 
 (setq fiplr-root-markers '(".git" ".svn"))
@@ -67,9 +82,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0cd56f8cd78d12fc6ead32915e1c4963ba2039890700458c13e12038ec40f6f5" "73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
  '(package-selected-packages
    (quote
-    (helm imenu-anywhere fiplr company diff-hl ivy swiper counsel avy molokai-theme))))
+    (youdao-dictionary ace-window helm imenu-anywhere fiplr company diff-hl ivy swiper counsel avy molokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
