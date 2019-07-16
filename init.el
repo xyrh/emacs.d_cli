9d71e57211d98afd5de28363302729ef1758b10b
huaxin <huaxin@xiongmaitech.com>
2019-07-16 08:55:32 +0800

    flycheck

--- init.el
+++ init.el
@@ -6,10 +6,12 @@
 (menu-bar-mode -1)
 (global-linum-mode t)
 (setq inhibit-splash-screen t)
+(setq auto-save-default nil)
 (setq make-backup-files nil)
 
 (electric-pair-mode t)
 (show-paren-mode t)
+(global-auto-revert-mode t)
 (global-diff-hl-mode)
 
 (set-language-environment 'UTF-8)
@@ -22,6 +24,7 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq mouse-yank-at-point t)
+(setq ring-bell-function 'ignore)
 
 (defun show-buffer-file-name ()
   (interactive)
@@ -48,6 +51,11 @@
 (avy-setup-default)
 (global-set-key (kbd "M-s a") 'avy-goto-char)
 (global-set-key (kbd "M-s l") 'avy-goto-line)
+
+(require 'popwin)
+(popwin-mode t)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
@@ -87,7 +95,7 @@
     ("0cd56f8cd78d12fc6ead32915e1c4963ba2039890700458c13e12038ec40f6f5" "73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
  '(package-selected-packages
    (quote
-    (youdao-dictionary ace-window helm imenu-anywhere fiplr company diff-hl ivy swiper counsel avy molokai-theme))))
+    (flycheck popwin youdao-dictionary ace-window helm imenu-anywhere fiplr company diff-hl ivy swiper counsel avy molokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
