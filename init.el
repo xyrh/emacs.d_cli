4ef090535557b27a6291b2424a1ae09176062e20
huaxin <huaxin@xiongmaitech.com>
2019-07-17 11:29:12 +0800

    mod key

--- init.el
+++ init.el
@@ -26,6 +26,9 @@
 (setq mouse-yank-at-point t)
 (setq ring-bell-function 'ignore)
 
+(global-set-key (kbd "C-a") 'mwim-beginning)
+(global-set-key (kbd "C-e") 'mwim-end)
+
 (defun show-buffer-file-name ()
   (interactive)
   (let ((file-name (buffer-file-name)))
@@ -41,16 +44,15 @@
 
 (global-set-key (kbd "M-o") 'ace-window)
 
-(global-set-key (kbd "C-\\") #'helm-imenu-anywhere)
+(global-set-key (kbd "M-\\") #'helm-imenu-anywhere)
 
 (setq fiplr-root-markers '(".git" ".svn"))
 (setq fiplr-ignored-globs '((directories (".git" ".svn"))
                             (files ("*.jpg" "*.png" "*.zip" "*~"))))
-(global-set-key (kbd "C-x f") 'fiplr-find-file)
+(global-set-key (kbd "C-\\") 'fiplr-find-file)
 
 (avy-setup-default)
-(global-set-key (kbd "M-s a") 'avy-goto-char)
-(global-set-key (kbd "M-s l") 'avy-goto-line)
+(global-set-key (kbd "M-/") 'avy-goto-char)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
@@ -60,7 +62,7 @@
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "M-;") 'counsel-ag)
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 (global-company-mode t)
@@ -76,6 +78,13 @@
             (when (derived-mode-p 'c-mode 'c++-mode)
               (ggtags-mode 1))))
 
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(require 'highlight-symbol)
+(global-set-key [f5] 'highlight-symbol)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -86,7 +95,7 @@
     ("0cd56f8cd78d12fc6ead32915e1c4963ba2039890700458c13e12038ec40f6f5" "73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
  '(package-selected-packages
    (quote
-    (ggtags flycheck youdao-dictionary ace-window helm imenu-anywhere fiplr company ivy swiper counsel avy molokai-theme))))
+    (mwim highlight-symbol neotree ggtags flycheck youdao-dictionary ace-window helm imenu-anywhere fiplr company ivy swiper counsel avy molokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
