e88a481bf885f4ee99834c2134562011e87418d6
huaxin <huaxin@xiongmaitech.com>
2019-07-17 09:50:17 +0800

    del diff-hl

--- init.el
+++ init.el
@@ -11,7 +11,6 @@
 (electric-pair-mode t)
 (show-paren-mode t)
 (global-auto-revert-mode t)
-(global-diff-hl-mode)
 
 (set-language-environment 'UTF-8)
 (set-locale-environment "UTF-8")
@@ -87,7 +86,7 @@
     ("0cd56f8cd78d12fc6ead32915e1c4963ba2039890700458c13e12038ec40f6f5" "73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
  '(package-selected-packages
    (quote
-    (ggtags flycheck youdao-dictionary ace-window helm imenu-anywhere fiplr company diff-hl ivy swiper counsel avy molokai-theme))))
+    (ggtags flycheck youdao-dictionary ace-window helm imenu-anywhere fiplr company ivy swiper counsel avy molokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
